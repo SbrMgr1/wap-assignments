548a7009aef04b5006bfec3e74089350ba998fce
sbr mgr <sbr.mgr1@gmail.com>
2020-06-04 00:46:49 -0500

    Added puzzle game

---
+++ bouncing-ball/assets/js/ball.js
@@ -0,0 +1,17 @@
+function update() {
+	ballVelocity += 1;
+	if (parseInt($("#ball").css('top')) > $(document).height()) {
+		ballVelocity *= -.9;
+	}
+	$("#ball").css('top', function(idx, old) { 
+		return parseInt(old) + ballVelocity + 'px';
+	}); 
+}
+var START = 0;
+var ballVelocity = 0;
+$(function() { 
+	$("#ball").css({
+    'top': '0px',
+	'left': $(window).width() / 2 + 'px' });
+  	setInterval(update, 20);
+});
